bf1a1a5f2013e94ec97113b36dbf216b5c21097f
Cristian Gonzalez <joel13gm@gmail.com>
2023-10-13 18:56:37 -0600

    update to 1.0 and 2.0

--- src/e01_arrays/Dummy.java
+++ src/e01_arrays/Dummy.java
@@ -5,9 +5,9 @@ public class Dummy {
   public static void main(String[] args) {
     System.out.println("FileToDelete");
     
-    System.out.println("change 1");
+    System.out.println("change 1.0");
     
-    System.out.println("change 2");
+    System.out.println("change 2.0");
   }
 
 }
